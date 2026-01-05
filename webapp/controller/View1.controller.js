eba370c61f040a2dad5ff825a6692c76aeb0ca7b
Gunjan Sharma <gunjan.designer25@gmail.com>
2026-01-05 08:00:15 +0000

    filter apply on search bar

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-     "shop/project1/model/models"
-], (Controller,models) => {
+     "shop/project1/model/models",
+     "sap/ui/model/Filter",
+     "sap/ui/model/FilterOperator"
+], (Controller,models,Filter,FilterOperator) => {
     "use strict";
 
     return Controller.extend("shop.project1.controller.View1", {
         onInit() {
+        },
+        onSearch: function(oEvent){
+            //step1:- what is the user type in search field
+           var sSearch = oEvent.getParameter("query");
+            //step2:- construct the filter object with operand and operator
+            var oFilter =  new Filter ("name" , FilterOperator.Contains ,sSearch);
+            var aFilter = [oFilter];
+            //step3:- get the list object
+            var oList = this.getView().byId("idLST");
+            //step4:- inject the filter to the list
+            oList.getBinding ("items").filter(aFilter);
         }
     });
 });
