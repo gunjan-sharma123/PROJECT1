7b684121f1c827fe2da3819770f0fcbfcf00138c
Gunjan Sharma <gunjan.designer25@gmail.com>
2026-01-08 12:41:13 +0000

    route match handler unit-24

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,6 +8,8 @@ sap.ui.define([
 
     return Controller.extend("shop.project1.controller.View1", {
         onInit() {
+            //its best practice to call the router inside oninit
+            this.Router = this.getOwnerComponent().getRouter();
         },
         onNext : function(){
             var oAppCon = this.getView().getParent();
@@ -22,6 +24,17 @@ sap.ui.define([
           aSelectedItems.forEach(item => {
             oList.removeItem(item);
           });
+        },
+        onFruitSelect: function(oEvent){
+              var oSelectedItem = oEvent.getParameter("listItem")
+            //step1- get the router object
+             this.Router
+            //step2- trigger the route
+             this.Router.navTo("second",
+                {
+                    fruitId: oSelectedItem.getBindingContextPath().split("/")[2]
+                }
+             );
         },
         onSearch: function(oEvent){
             //step1:- what is the user type in search field
--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -6,6 +6,15 @@ sap.ui.define([
 
     return Controller.extend("shop.project1.controller.View2", {
         onInit() {
+            this.oRouter = this.getOwnerComponent().getRouter();
+            this.oRouter.getRoute("second").attachPatternMatched(this.herculis, this);
+
+        },
+        herculis: function(oEvent){
+            debugger;
+            var fruitId = oEvent.getParameter("arguments").fruitId;
+            var sPath = '/fruits/' + fruitId;
+            this.getView().bindElement(sPath);
         },
         onBack: function(){
               var oAppCon = this.getView().getParent();
